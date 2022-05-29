5de5396da9a10315151b6b8755461c367fae3cb9
LoiseMulwa <loisemulwa7@gmail.com>
2022-05-30 00:24:46 +0300

    ft:Add settings import

--- galleryapp/urls.py
+++ galleryapp/urls.py
@@ -17,9 +17,15 @@ Including another URLconf
 # from django.conf.urls import url,include
 from django.contrib import admin
 from django.urls import re_path as url,include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'',include('photos.urls'))
     # re_path(r'^photos/',include('photos.urls'))
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+ 
