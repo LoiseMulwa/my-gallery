b986728f02f0121b890235b82d1267fcf3d42e39
LoiseMulwa <loisemulwa7@gmail.com>
2022-05-29 22:04:37 +0300

    ft:Database migrations

--- galleryapp/settings.py
+++ galleryapp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,8 +78,10 @@ WSGI_APPLICATION = 'galleryapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'gallery',
+        'USER': 'moringa',
+        'PASSWORD': '1234',
     }
 }
 
@@ -107,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,19 @@
+from email.mime import image
+from unicodedata import category
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False)
+
+    def __str__(self):
+        return self.name
+
+class Photo(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    description = models.TextField()
+    image = models.ImageField(max_length=500, null=False, blank=False)
+
+    def __str__(self):
+        return self.description
+
