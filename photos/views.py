8b16e693c730487c3f30c5d52f19529b2e8558f8
LoiseMulwa <loisemulwa7@gmail.com>
2022-05-30 00:23:26 +0300

    ft: Add a new regex for linking images

--- photos/views.py
+++ photos/views.py
@@ -1,11 +1,16 @@
+from multiprocessing import context
+from unicodedata import category
 from django.shortcuts import render
+from .models import Category, Photo
 # from django.http  import HttpResponse
 
 
 # Create your views here.
 def gallery(request):
+    categories = Category.objects.all()
+    context = {'categories': categories}
     return render(request, 'photos/gallery.html')
-    
+
 def viewPhoto(request):
     return render(request,'photos/photo.html')
 
