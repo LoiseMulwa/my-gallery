ea417ec457aac9ca39ed73e73e031cda7927f7b0
LoiseMulwa <loisemulwa7@gmail.com>
2022-05-30 03:54:10 +0300

    ft:Add link to add.html template

--- photos/views.py
+++ photos/views.py
@@ -10,19 +10,23 @@ from .models import Category, Photo
 
 # Create your views here.
 # def put(self, request, *args, **kwargs):
-def gallery(request, self, *args, **kwargs):
-    pk = self.kwargs.get('pk')
+# def gallery(request, self, *args, **kwargs):
+    # pk = self.kwargs.get('pk')
+def gallery(request):
     categories = Category.objects.all()
     photos = Photo.objects.all()
     context = {'categories': categories, 'photos': photos}
     return render(request, 'photos/gallery.html' , context)
 
-def viewPhoto(request, pk):
-    photo = Photo.objects.get(id=pk)
+def viewPhoto(request):
+    photo = Photo.objects.all()
     return render(request, 'photos/photo.html',{'photos': photo} )
     
 
 def addPhoto(request):
-    return render(request,'photos/add.html')
+    categories = Category.objects.all()
+
+    context = {'categories': categories}
+    return render(request,'photos/add.html', context)
 
 
