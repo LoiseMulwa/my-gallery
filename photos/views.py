985126f7828a17f23c3c8062d3c42661891d23cc
LoiseMulwa <loisemulwa7@gmail.com>
2022-05-31 10:57:29 +0300

    ft:Modify views in photos

--- photos/views.py
+++ photos/views.py
@@ -23,13 +23,13 @@ def gallery(request):
 
 
     categories = Category.objects.all()
-    # photos = Photo.objects.all()
+    photos = Photo.objects.all()
     context = {'categories': categories, 'photos': photos}
     return render(request, 'photos/gallery.html' , context)
 
 def viewPhoto(request, pk):
     photo = Photo.objects.get(id=pk)
-    return render(request, 'photos/photo.html',{'photos': photo} )
+    return render(request, 'photos/photo.html',{'photo': photo} )
     
 
 def addPhoto(request):
